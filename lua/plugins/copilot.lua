34dec41cb5bb4cfb8d7c0071914444e5e3024e12
Connor Dillon <cdillon@scalecomputing.com>
2024-05-01 18:00:04 -0700

    Cronjob Backup 05_01_2024_18_00_01

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,8 +6,8 @@ return {
   -- ft = { 'go', 'python', 'javascript', 'typescript', 'rust', 'c', 'cpp', 'lua' },
 
   config = function()
-    vim.keymap.set("n", "<leader>cpe", "<cmd>Copilot enable<CR>");
-    vim.keymap.set("n", "<leader>cpd", "<cmd>Copilot disable<CR>");
+    vim.keymap.set("n", "<leader>cpe", "<cmd>Copilot enable<CR>", { desc = "[c]o[p]ilot [e]nable" });
+    vim.keymap.set("n", "<leader>cpd", "<cmd>Copilot disable<CR>", { desc = "[c]o[p]ilot [d]isable" });
     -- vim.cmd("let g:copilot_filetypes = {'*': v:false}") -- disable permanently
     -- vim.cmd([[let g:copilot_workspace_folders = {'~/dev/scale-qe'}]])
     -- vim.cmd(":echo keys(copilot#Agent().workspaceFolders)")
