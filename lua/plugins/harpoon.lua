2df0031d282f36998b0d5de4a3721711fd035c94
Connor Dillon <cdillon@scalecomputing.com>
2024-10-09 18:00:03 -0700

    Cronjob Backup 10_09_2024_18_00_01

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,23 +1,23 @@
 return {
-  "theprimeagen/harpoon",
-  config = function()
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
-
-    vim.keymap.set("n", "<leader>AH", mark.add_file)
-    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
-    vim.keymap.set("n", "<M-h>", function() ui.nav_file(1) end)
-    vim.keymap.set("n", "<M-j>", function() ui.nav_file(2) end)
-    vim.keymap.set("n", "<M-k>", function() ui.nav_file(3) end)
-    vim.keymap.set("n", "<M-l>", function() ui.nav_file(4) end)
-    vim.keymap.set("n", "<M-n>", function() ui.nav_file(5) end)
-    vim.keymap.set("n", "<M-m>", function() ui.nav_file(6) end)
-    vim.keymap.set("n", "<M-,>", function() ui.nav_file(7) end)
-    vim.keymap.set("n", "<M-.>", function() ui.nav_file(8) end)
-    vim.keymap.set("n", "<M-y>", function() ui.nav_file(9) end)
-    vim.keymap.set("n", "<M-u>", function() ui.nav_file(10) end)
-    vim.keymap.set("n", "<M-i>", function() ui.nav_file(11) end)
-    vim.keymap.set("n", "<M-o>", function() ui.nav_file(12) end)
-  end
+  -- "theprimeagen/harpoon",
+  -- config = function()
+  --   local mark = require("harpoon.mark")
+  --   local ui = require("harpoon.ui")
+  --
+  --   vim.keymap.set("n", "<leader>AH", mark.add_file)
+  --   vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+  --
+  --   vim.keymap.set("n", "<M-h>", function() ui.nav_file(1) end)
+  --   vim.keymap.set("n", "<M-j>", function() ui.nav_file(2) end)
+  --   vim.keymap.set("n", "<M-k>", function() ui.nav_file(3) end)
+  --   vim.keymap.set("n", "<M-l>", function() ui.nav_file(4) end)
+  --   vim.keymap.set("n", "<M-n>", function() ui.nav_file(5) end)
+  --   vim.keymap.set("n", "<M-m>", function() ui.nav_file(6) end)
+  --   vim.keymap.set("n", "<M-,>", function() ui.nav_file(7) end)
+  --   vim.keymap.set("n", "<M-.>", function() ui.nav_file(8) end)
+  --   vim.keymap.set("n", "<M-y>", function() ui.nav_file(9) end)
+  --   vim.keymap.set("n", "<M-u>", function() ui.nav_file(10) end)
+  --   vim.keymap.set("n", "<M-i>", function() ui.nav_file(11) end)
+  --   vim.keymap.set("n", "<M-o>", function() ui.nav_file(12) end)
+  -- end
 }
